b56f5249f887a09b0d26344a58a70e7eafdcaa9b
Ben Phelps <github@splopp.com>
2016-06-29 09:38:02 +1000

    feat: Add fizzbuzz page

--- 03 Multi User Blog/multiuserblog/main.py
+++ 03 Multi User Blog/multiuserblog/main.py
@@ -8,9 +8,15 @@ from src.route import Handler
 class IndexHandler(Handler):
     def get(self):
         self.render("shopping.html")
+
+class FizzbuzzHandler(Handler):
+    def get(self):
+        self.render("fizzbuzz.html")
+
         
 app = webapp2.WSGIApplication([
-    ('/', IndexHandler)
+    ('/', IndexHandler),
+    ('/fizzbuzz', FizzbuzzHandler),
 ], debug=True)
 
 
