38994977baf7db68b72bfe03b7e06438dcb9da0c
Ben Phelps <github@splopp.com>
2016-08-03 13:01:17 +1000

    style: Update dbextensions.py and security.py to conform to PEP8 standards

--- 03 Multi User Blog/multiuserblog/src/dbextensions.py
+++ 03 Multi User Blog/multiuserblog/src/dbextensions.py
@@ -1,7 +1,10 @@
 from google.appengine.ext import db
 
+
 def get_user_from_username(username):
-    query = db.GqlQuery("SELECT * FROM SiteUser WHERE username = '%s'" % username.lower())
-    
+    query = db.GqlQuery(
+        ("SELECT * FROM SiteUser"
+         "WHERE username = '%s'") % username.lower())
+
     # Retrieves first valid row, or None
     return query.get()
--- 03 Multi User Blog/multiuserblog/src/security.py
+++ 03 Multi User Blog/multiuserblog/src/security.py
@@ -3,25 +3,32 @@ import string
 import hashlib
 import random
 
-# use bcrypt, not sha256.
+
+# Should be stored in another location (so it is not shared)
+SECRET = 'imsosecret'
+
 
 def valid_pw(name, pw, hash):
     salt = hash.split(",")[1]
     return hash == make_pw_hash(name, pw, salt)
 
+
 def make_pw_hash(name, pw, salt):
     hash = hashlib.sha256(name.lower() + pw + salt).hexdigest()
     return "%s,%s" % (hash, salt)
 
+
 def make_salt():
     return ''.join(random.choice(string.letters) for _ in range(5))
 
-SECRET = 'imsosecret' # Make it a long string of random chars stored in another location (so it is not shared)
+
 def hash_str(s):
     return hmac.new(SECRET, s).hexdigest()
 
+
 def make_cookie_data(value):
     return "%s|%s" % (value, hash_str(value))
+
 
 def cookie_is_valid(cookie):
     if cookie is not None:
